ce00a4efe5987d0f229ca12f0e5d63bfa91e4eef
Harshit <hkothari2006@gmail.com>
2026-07-13 12:21:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (7.42%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_map<char,int> mp = {
+            {'a',0},
+            {'b',0},
+            {'c',0},
+            {'d',0},
+            {'e',0},
+            {'f',0},
+            {'g',0},
+            {'h',0},
+            {'i',0},
+            {'j',0},
+            {'k',0},
+            {'l',0},
+            {'m',0},
+            {'n',0},
+            {'o',0},
+            {'p',0},
+            {'q',0},
+            {'r',0},
+            {'s',0},
+            {'t',0},
+            {'u',0},
+            {'v',0},
+            {'w',0},
+            {'x',0},
+            {'y',0},
+            {'z',0}
+        };
+        for (char ch : sentence){
+            mp[ch]++;
+        }
+        for (auto it : mp){
+            if (it.second==0)
+                return false;
+        }
+        return true;
+    }
+};
